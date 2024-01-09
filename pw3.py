2fbd9d79b08bde32c8e48438da5017cfbd9f9884
minji-km <minji.kmk@gmail.com>
2024-01-09 10:38:57 +0100

    Update pw3.py

--- pw3.py
+++ pw3.py
@@ -1,27 +1,106 @@
+import pandas as pd
+pd.options.mode.chained_assignment = None 
+import numpy as np
+import re
+import nltk
+nltk.download('stopwords')
+from gensim.models import word2vec
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
 import streamlit as st
-import visualisation
 
-# Charger les données
-data = visualisation.data
+data = pd.read_csv('train.csv').sample(50000, random_state=23)
+STOP_WORDS = nltk.corpus.stopwords.words()
 
-# Afficher les 5 premières lignes de données
+def clean_sentence(val):
+    "remove chars that are not letters or numbers, downcase, then remove stop words"
+    regex = re.compile('([^\s\w]|_)+')
+    sentence = regex.sub('', val).lower()
+    sentence = sentence.split(" ")
+    
+    for word in list(sentence):
+        if word in STOP_WORDS:
+            sentence.remove(word)  
+            
+    sentence = " ".join(sentence)
+    return sentence
+
+def clean_dataframe(data):
+    "drop nans, then apply 'clean_sentence' function to question1 and 2"
+    data = data.dropna(how="any")
+    
+    for col in ['question1', 'question2']:
+        data[col] = data[col].apply(clean_sentence)
+    
+    return data
+
+data = clean_dataframe(data)
 st.write(data.head(5))
 
-# Construire le corpus
-corpus = visualisation.corpus
+def build_corpus(data):
+    "Creates a list of lists containing words from each sentence"
+    corpus = []
+    for col in ['question1', 'question2']:
+        for sentence in data[col].items():
+        #for sentence in data[col]:
+            #print(sentence)
+            word_list = sentence[1].split(" ")
+            corpus.append(word_list)
+            
+    return corpus
 
-# Afficher les 2 premiers éléments du corpus
+
+st.write(data.head(5))
+
+corpus = build_corpus(data)        
+corpus[0:2]
+
 st.write(corpus[0:2])
 
-# Construire le modèle word2vec
-model = visualisation.model
 
-# Afficher le vecteur pour 'trump'
+model = word2vec.Word2Vec(corpus, vector_size=100, window=20, min_count=200, workers=4)
 st.write(model.wv['trump'])
 
-# Construire le vocabulaire
-vocab = visualisation.vocab
+vocab = []
+for i in range(0,len(model.wv)):
+        vocab.append(model.wv.index_to_key[i])
 
-# Afficher le plot TSNE
-fig = visualisation.tsne_plot(model)
+
+
+def tsne_plot(model):
+    "Creates and TSNE model and plots it"
+    labels = []
+    tokens = []
+
+    for word in vocab:
+        tokens.append(model.wv[word])
+        labels.append(word)
+        #print(tokens)
+        #print(labels)
+    tokens = np.array(tokens)
+    tsne_model = TSNE(perplexity=40, n_components=2, init='pca', n_iter=2500, random_state=23)
+    new_values = tsne_model.fit_transform(tokens)
+
+
+    
+    x = []
+    y = []
+    for value in new_values:
+        x.append(value[0])
+        y.append(value[1])
+    
+    
+    
+    plt.figure(figsize=(16, 16)) 
+    for i in range(len(x)):
+        plt.scatter(x[i],y[i])
+        plt.annotate(labels[i],
+                     xy=(x[i], y[i]),
+                     xytext=(5, 2),
+                     textcoords='offset points',
+                     ha='right',
+                     va='bottom')
+    plt.show()
+
+fig = tsne_plot(model)
 st.pyplot(fig)
